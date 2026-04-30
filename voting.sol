12de30c5929af035383b4912c6dfbcb2b4075051
WasiG-NYU <wg2275@nyu.edu>
2026-04-30 17:39:25 -0400

    Added some comments 3

--- voting.sol
+++ voting.sol
@@ -19,13 +19,15 @@ contract MyVoting { // Define the smart contract
 
     // Sets the owner and voting duration 
     constructor(uint _durationMin) {
-        owner = msg.sender;
-        votingEnd = block.timestamp + (_durationMin * 1 minutes);
+        owner = msg.sender; // Sets the deployer of the contract as the owner
+        votingEnd = block.timestamp + (_durationMin * 1 minutes); 
+        // Sets the voting time based on current time + duration
     }
 
     // Only the owner can add cadidates
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can call this!");
+        // Reverts if the caller is not the owner 
         _; 
     }
 
@@ -33,14 +35,15 @@ contract MyVoting { // Define the smart contract
     function addCandidate(string memory _name) public onlyOwner {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+        // Creates and stores a new candidate with 0 initial votes 
     }
 
     // Voting function 
-    function vote(uint _candidateId) public {
-        require(block.timestamp < votingEnd, "Voting time has ended.");
-        require(!hasVoted[msg.sender], "You have already voted!");
-        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate.");
-        hasVoted[msg.sender] = true; 
-        candidates[_candidateId].voteCount++;
+    function vote(uint _candidateId) public { // Allows a user to vote for a candidate by ID
+        require(block.timestamp < votingEnd, "Voting time has ended."); // Makes sure that voting is still ongoing
+        require(!hasVoted[msg.sender], "You have already voted!"); // Makes sure that sender has not voted before
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate."); // Validates candidate ID
+        hasVoted[msg.sender] = true; // Marks sender has having voted
+        candidates[_candidateId].voteCount++; // Increments the vote count for selected candidate
     }
 }
